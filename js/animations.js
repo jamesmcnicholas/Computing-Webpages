347a8785164ca128cb888a8de3291f78e606675f
Unknown <jmcnicholas.62743@farnborough.ac.uk>
2018-03-11 19:40:50 +0000

    Finished Data > Advanced boolean algebra, Boolean Applications. Fixed scrolling on homepage, minor alterations.

--- js/animations.js
+++ js/animations.js
@@ -8,7 +8,21 @@
  *
  * Version: 0.3.3
  */
-(function(e){function u(){r=false;for(var n=0;n<t.length;n++){var i=e(t[n]).filter(function(){return e(this).is(":appeared")});i.trigger("appear",[i]);if(o){var s=o.not(i);s.trigger("disappear",[s])}o=i}}var t=[];var n=false;var r=false;var i={interval:250,force_process:false};var s=e(window);var o;e.expr[":"]["appeared"]=function(t){var n=e(t);if(!n.is(":visible")){return false}var r=s.scrollLeft();var i=s.scrollTop();var o=n.offset();var u=o.left;var a=o.top;if(a+n.height()>=i&&a+(n.data("appear-bottom-offset")||0)<=i+s.height()&&u+n.width()>=r&&u-(n.data("appear-left-offset")||0)<=r+s.width()){return true}else{return false}};e.fn.extend({appear:function(s){var o=e.extend({},i,s||{});var a=this.selector||this;if(!n){var f=function(){if(r){return}r=true;setTimeout(u,o.interval)};e(window).scroll(f).resize(f);n=true}if(o.force_process){setTimeout(u,o.interval)}t.push(a);return e(a)}});e.extend({force_appear:function(){if(n){u();return true}return false}})})(jQuery)
+(function(e){function u(){r=false;for(var n=0;n<t.length;n++){
+  var i=e(t[n]).filter(
+  function(){return e(this).is(":appeared")
+});
+i.trigger("appear",[i]);
+if(o){var s=o.not(i);s.trigger("disappear",[s])}o=i}}var t=[];var n=false;
+var r=false;var i={interval:250,force_process:false};var s=e(window);
+var o;e.expr[":"]["appeared"]=function(t){var n=e(t);
+  if(!n.is(":visible")){return false}var r=s.scrollLeft();
+  var i=s.scrollTop();
+  var o=n.offset();
+  var u=o.left;var a=o.top;
+  if(a+n.height()>=i&&a+(n.data("appear-bottom-offset")||0)<=i+s.height()&&u+n.width()>=r&&u-(n.data("appear-left-offset")||0)<=r+s.width()){return true}else{return false}};
+  e.fn.extend({appear:function(s){var o=e.extend({},i,s||{});
+  var a=this.selector||this;if(!n){var f=function(){if(r){return}r=true;setTimeout(u,o.interval)};e(window).scroll(f).resize(f);n=true}if(o.force_process){setTimeout(u,o.interval)}t.push(a);return e(a)}});e.extend({force_appear:function(){if(n){u();return true}return false}})})(jQuery)
 
 
 //----ANIMATIONS---//
@@ -40,8 +54,8 @@ jQuery('.animated').appear();
 	jQuery(document.body).on('appear', '.fadeInRightNow', function() {
         jQuery(this).each(function(){ jQuery(this).addClass('fadeInRight') });
     });
-	
-	
+
+
 	jQuery(document.body).on('appear', '.fadeInUpBigNow', function() {
     jQuery(this).each(function(){ jQuery(this).addClass('fadeInUpBig') });
     });
@@ -54,7 +68,7 @@ jQuery('.animated').appear();
 	jQuery(document.body).on('appear', '.fadeInRightBigNow', function() {
         jQuery(this).each(function(){ jQuery(this).addClass('fadeInRightBig') });
     });
-	
+
 	jQuery(document.body).on('appear', '.fadeInNow', function() {
         jQuery(this).each(function(){ jQuery(this).addClass('fadeIn') });
     });
